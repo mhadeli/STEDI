47528618f21791799053e87f3f68bbf08fb67fbb
Hussain Adeli <58530203+hussainadeli@users.noreply.github.com>
2023-10-12 13:19:55 +0200

    customer_landing.sql
    create customer landing table

---
+++ customer_landing.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi_db`.`customer_trusted` (
+  `customerName` string,
+  `email` string,
+  `phone` int,
+  `birthDay` date,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `lastUpdateDate` bigint,
+  `shareWithResearchAsOfDate` bigint,
+  `shareWithPublicAsOfDate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://stedi-project-s3/customer_landing/'
+TBLPROPERTIES ('classification' = 'json');
